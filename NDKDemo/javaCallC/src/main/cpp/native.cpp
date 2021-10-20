dcd33aa6f3351772cf964c1e463f60f0fde6c373
28188 <2818815189@qq.com>
2021-10-20 22:26:40 +0800

    第一次提交

--- NDKDemo/javaCallC/src/main/cpp/native.cpp
+++
@@ -1,7 +0,0 @@
-#include "native.h"
-
-extern "C" jstring Java_com_kangwang_javacallc_Demo01_stringFromJNI(JNIEnv *env, jobject thiz) {
-    // TODO: implement stringFromJNI()
-    std::string hello = "Hel";
-    return env->NewStringUTF(hello.c_str());
-}
---
+++ cmakedemo/cmakedemo/cmakedemo.cpp
@@ -0,0 +1,12 @@
+﻿// cmakedemo.cpp: 定义应用程序的入口点。
+//
+
+#include "cmakedemo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
